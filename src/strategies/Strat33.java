b5d053b66d97fecc4515e64b93ee4de85d92ae97
Alexis Aoun <pizzaman@localhost.localdomain>
2021-04-25 21:18:35 +0200

    doc: refactored and added in code documentation

--- src/strategies/Strat33.java
+++ src/strategies/Strat33.java
@@ -3,13 +3,25 @@ package strategies;
 import cantstop.Jeu;
 
 /**
- * Votre Stratégie (copie de la Strat0 pour l'instant)
- *
- * Expliquez votre stratégie en une 20aine de lignes maximum.
- *
- * RENDU: Ce fichier, correctement nommé et rempli avec le numéro de la
- * stratégie choisie.
- *
+ * Pour le choix : On regarde la probabilité de chaque choix (proba calculées à
+ * l'initialisation) a partir de ces probas et d'un poids affecté à chaque
+ * colonne (qui est l'inverse de la fréquence d'obtention du numero de cette
+ * colonne) on évalue a chaque choix un score de formule : 
+ * 
+ * (proba de finir colonne 1 + proba de finir colonne 2) * 1000. 
+ * 
+ * On affecte aussi une pénalité
+ * (division par 1.5) au proba des colonnes où un pion adverse est déjà présent
+ * et a une avance (nb de cases d'avance * le poids de la colonne). On prend le choix 
+ * avec le score le plus grand score.
+ * Pour le stop : on s'arrete si on arrive au max d'une colonne
+ * on ne s'arrete pas tant que tous les bonzes ont été placés.
+ * Si tous les bonzes ont été placés on calcule l'espérance (ou le progres 
+ * attendu au prochain tour) et on le compare au progres actuel fait durant 
+ * le tour en cour : en résumé on continue tant que 
+ * 
+ * progresAttendu >= progresActuel/coefEsperance 
+ * 
  * @author AOUN/OURIACHI
  */
 public class Strat33 implements Strategie {
@@ -17,7 +29,6 @@ public class Strat33 implements Strategie {
     private double probaSingle[], freq[], weigth[];
     private double probaCouple[][];
     private double probaDouble[];
-    private double probaTriple[][][];
 
     private double coefEsperance = 9.0;
 
@@ -29,7 +40,6 @@ public class Strat33 implements Strategie {
         this.weigth = new double[13];
         this.probaCouple = new double[13][13];
         this.probaDouble = new double[13];
-        this.probaTriple = new double[13][13][13];
 
         computeProba();
     }
@@ -58,11 +68,6 @@ public class Strat33 implements Strategie {
                             for (int j = i + 1; j <= 12; j++) {
                                 if (isCoupleInSum(sumD, i, j))
                                     this.probaCouple[i][j]++;
-
-                                for (int k = j + 1; k <= 12; k++) {
-                                    if (isOneOfThreeInSum(sumD, i, j, k))
-                                        this.probaTriple[i][j][k]++;
-                                }
                             }
                         }
 
@@ -84,15 +89,18 @@ public class Strat33 implements Strategie {
             for (int j = i + 1; j <= 12; j++) {
 
                 this.probaCouple[i][j] /= nbTotalIssues;
-
-                for (int k = j + 1; k <= 12; k++)
-                    this.probaTriple[i][j][k] /= nbTotalIssues;
-
             }
         }
     }
 
-    // Fonction qui return un tableau avec toutes les sommes possbiles de 4 des
+    /**
+     * 
+     * @param a de 1
+     * @param b de 2
+     * @param c de 3
+     * @param d de 4
+     * @return les sommes possibles avec les 4 des obetnus
+     */
     public int[][] computeSumD(int a, int b, int c, int d) {
         int[][] output = new int[3][2];
 
@@ -106,8 +114,12 @@ public class Strat33 implements Strategie {
         return output;
     }
 
-    // Fonction qui retourne le nombre de fois que n est present dans le tableau de
-    // somme
+    /**
+     * 
+     * @param sumD le tableau de sommes
+     * @param n le chiffre recherché
+     * @return le nombre de fois que n apparait dans les sommes des dés
+     */
     public int computeOccurences(int[][] sumD, int n) {
         int output = 0;
         for (int i = 0; i < 3; i++) {
@@ -121,7 +133,13 @@ public class Strat33 implements Strategie {
         return output;
     }
 
-    // Fonction qui verifie si un couple d entier se trouve dans le tableau de somme
+    /**
+     * 
+     * @param sumD le tableau de sommes
+     * @param n1 le premier chiffre du couple 
+     * @param n2 le second chiffre du couple
+     * @return si vrai ou faux le couple est present dans les sommes
+     */
     public boolean isCoupleInSum(int[][] sumD, int n1, int n2) {
         boolean output = false;
         for (int i = 0; i < 3; i++) {
@@ -131,26 +149,10 @@ public class Strat33 implements Strategie {
         return output;
     }
 
-    public boolean isOneOfTwoInSum(int[][] sumD, int n1, int n2) {
-        boolean output = false;
-        if ((computeOccurences(sumD, n1) != 0) || (computeOccurences(sumD, n2) != 0))
-            output = true;
-        return output;
-    }
-
-    // Fonction qui verifie que au moins une des trois valeurs est presente dans le
-    // tableau de somme
-    public boolean isOneOfThreeInSum(int[][] sumD, int n1, int n2, int n3) {
-        boolean output = false;
-        if (((computeOccurences(sumD, n1) != 0) || (computeOccurences(sumD, n2) != 0))
-                || (computeOccurences(sumD, n3) != 0))
-            output = true;
-        return output;
-    }
 
     /**
      * @param j le jeu
-     * @return
+     * @return la décision de l'IA
      */
     public int choix(Jeu j) {
 
@@ -173,6 +175,7 @@ public class Strat33 implements Strategie {
 
             double score = 0;
 
+            //calcul des probas et des pénalités 
             int[][] choix = j.getLesChoix();
             int[] avancementActuel = avancementActuel(j.avancementJoueurEnCours(), j.getBonzes());
             int[] avancementAdverse = j.avancementAutreJoueur();
@@ -185,16 +188,17 @@ public class Strat33 implements Strategie {
                 int casesRestantesAdvC2 = nbCases[choix[i][1] - 2] - avancementAdverse[choix[i][1] - 2];
                 int casesRestantesC2 = nbCases[choix[i][1] - 2] - avancementActuel[choix[i][1] - 2];
 
-                if (casesRestantesAdvC2 * weigth[choix[i][1]] - casesRestantesC2 * weigth[choix[i][1]] > 10)
+                if (casesRestantesAdvC2 * weigth[choix[i][1]] - casesRestantesC2 * weigth[choix[i][1]] > 6)
                     probaColonne2 /= 1.5;
             }
 
             int casesRestantesAdvC1 = nbCases[choix[i][0] - 2] - avancementAdverse[choix[i][0] - 2];
             int casesRestantesC1 = nbCases[choix[i][0] - 2] - avancementActuel[choix[i][0] - 2];
 
-            if (casesRestantesAdvC1 * weigth[choix[i][0]] - casesRestantesC1 * weigth[choix[i][0]] > 10)
+            if (casesRestantesAdvC1 * weigth[choix[i][0]] - casesRestantesC1 * weigth[choix[i][0]] > 6)
                 probaColonne1 /= 1.5;
 
+            //calcul du score
             score = (probaColonne1 + probaColonne2) * 1000;
 
             if (this.debug) {
@@ -203,6 +207,7 @@ public class Strat33 implements Strategie {
                 System.out.println("Score du choix = " + score + " scoreMax = " + scoreMax);
             }
 
+            //on prend le choix avec le scoer le plus grand
             if (score >= scoreMax) {
                 scoreMax = score;
                 output = i;
@@ -216,6 +221,13 @@ public class Strat33 implements Strategie {
         return output;
     }
 
+    /**
+     * 
+     * @param j le jeu 
+     * @param colonne la colonne 
+     * @param avancement l'avancement sur la colonne
+     * @return la probabilité de finir la colonne
+     */
     public double probaFinirUneColonne(Jeu j, int colonne, int avancement) {
         double output = 0;
         int casesRestantes = j.getMaximum()[colonne - 2] - avancement;
@@ -225,17 +237,23 @@ public class Strat33 implements Strategie {
         return output;
     }
 
+    /**
+     * 
+     * @param avancement l'avancement des jetons
+     * @param bonzes l'avancement des bonzes
+     * @return un tableau de la somme de l'avancement des bonzes et celui des jetons
+     */
     public int[] avancementActuel(int[] avancement, int[][] bonzes) {
         int[] output = new int[11];
-    
+
         for (int i = 0; i < avancement.length; i++) {
             for (int j = 0; j < 3; j++) {
-                if (bonzes[j][0] == i + 2) 
+                if (bonzes[j][0] == i + 2)
                     output[i] = avancement[i] + bonzes[j][1];
             }
         }
 
-        if(this.debug){
+        if (this.debug) {
             System.out.println("----- Debug avancementActuel------");
             afficherTabInt(output);
         }
@@ -244,30 +262,31 @@ public class Strat33 implements Strategie {
 
     /**
      * @param j le jeu
-     * @return toujours vrai (pour s'arrêter)
+     * @return si vrai ou faux la stratégie décide de s'arreter
      */
     public boolean stop(Jeu j) {
-
+        
+        //on verifie qu on a pas atteint un maximum
         boolean[] colonnesBloquees = j.getBloque();
         for (int i = 0; i < colonnesBloquees.length; i++) {
             if (colonnesBloquees[i])
                 return true;
         }
-
+        //on verifie qu on a bien utilisé tous nos bonzes
         if (j.getBonzesRestants() != 0)
             return false;
 
         boolean output = true;
 
         double esperance = 0;
-        double progresActuelle = 0;
+        double progresActuel = 0;
 
         int[][] bonzes = j.getBonzes();
         double[][] avancement = new double[3][2];
 
-        for(int i = 0; i<3; i++){
-            for(int k = 0; k<2; k++)
-                avancement[i][k] = (double)bonzes[i][k];
+        for (int i = 0; i < 3; i++) {
+            for (int k = 0; k < 2; k++)
+                avancement[i][k] = (double) bonzes[i][k];
         }
 
         if (this.debug) {
@@ -275,14 +294,16 @@ public class Strat33 implements Strategie {
 
         }
 
-        progresActuelle = (double) avancement[0][0] * weigth[bonzes[0][1]]
-                + (double) avancement[1][0] * weigth[bonzes[1][1]]
-                + (double) avancement[2][0] * weigth[bonzes[2][1]];
+        //on calcule le proges actuel et le progres attendu au prochain tour (esperance)
+
+        progresActuel = (double) avancement[0][0] * weigth[bonzes[0][1]]
+                + (double) avancement[1][0] * weigth[bonzes[1][1]] + (double) avancement[2][0] * weigth[bonzes[2][1]];
 
         esperance =
 
-                this.probaSingle[bonzes[0][1]] * ((avancement[0][0] + 1) * weigth[bonzes[0][1]]
-                        + avancement[1][0] * weigth[bonzes[1][1]] + avancement[2][0] * weigth[bonzes[2][1]])
+                this.probaSingle[bonzes[0][1]]
+                        * ((avancement[0][0] + 1) * weigth[bonzes[0][1]] + avancement[1][0] * weigth[bonzes[1][1]]
+                                + avancement[2][0] * weigth[bonzes[2][1]])
 
                         +
 
@@ -292,34 +313,30 @@ public class Strat33 implements Strategie {
 
                         +
 
-                        this.probaSingle[bonzes[2][1]] * (avancement[0][0] * weigth[bonzes[0][1]]
+                        this.probaSingle[bonzes[2][1]]
+                                * (avancement[0][0] * weigth[bonzes[0][1]] + avancement[1][0] * weigth[bonzes[1][1]]
+                                        + (avancement[2][0] + 1) * weigth[bonzes[2][1]])
+
+                        +
+
+                        this.probaCouple[bonzes[0][1]][bonzes[1][1]] * ((avancement[0][0] + 1) * weigth[bonzes[0][1]]
+                                + (avancement[1][0] + 1) * weigth[bonzes[1][1]]
+                                + avancement[2][0] * weigth[bonzes[2][1]])
+                        +
+
+                        this.probaCouple[bonzes[0][1]][bonzes[2][1]] * ((avancement[0][0] + 1) * weigth[bonzes[0][1]]
                                 + avancement[1][0] * weigth[bonzes[1][1]]
+                                + (avancement[2][0] + 1) * weigth[bonzes[2][1]])
+                        +
+
+                        this.probaCouple[bonzes[1][1]][bonzes[2][1]] * (avancement[0][0] * weigth[bonzes[0][1]]
+                                + (avancement[1][0] + 1) * weigth[bonzes[1][1]]
                                 + (avancement[2][0] + 1) * weigth[bonzes[2][1]])
 
                         +
 
-                        this.probaCouple[bonzes[0][1]][bonzes[1][1]]
-                                * ((avancement[0][0] + 1) * weigth[bonzes[0][1]]
-                                        + (avancement[1][0] + 1) * weigth[bonzes[1][1]]
-                                        + avancement[2][0] * weigth[bonzes[2][1]])
-                        +
-
-                        this.probaCouple[bonzes[0][1]][bonzes[2][1]]
-                                * ((avancement[0][0] + 1) * weigth[bonzes[0][1]]
-                                        + avancement[1][0] * weigth[bonzes[1][1]]
-                                        + (avancement[2][0] + 1) * weigth[bonzes[2][1]])
-                        +
-
-                        this.probaCouple[bonzes[1][1]][bonzes[2][1]]
-                                * (avancement[0][0] * weigth[bonzes[0][1]]
-                                        + (avancement[1][0] + 1) * weigth[bonzes[1][1]]
-                                        + (avancement[2][0] + 1) * weigth[bonzes[2][1]])
-
-                        +
-
                         this.probaDouble[bonzes[0][1]] * ((bonzes[0][0] + 2) * weigth[bonzes[0][1]]
-                                + avancement[1][0] * weigth[bonzes[1][1]]
-                                + avancement[2][0] * weigth[bonzes[2][1]])
+                                + avancement[1][0] * weigth[bonzes[1][1]] + avancement[2][0] * weigth[bonzes[2][1]])
 
                         +
 
@@ -329,16 +346,16 @@ public class Strat33 implements Strategie {
 
                         +
 
-                        this.probaDouble[bonzes[2][1]] * (avancement[0][0] * weigth[bonzes[0][1]]
-                                + avancement[1][0] * weigth[bonzes[1][1]]
-                                + (avancement[2][0] + 2) * weigth[bonzes[2][1]]);
+                        this.probaDouble[bonzes[2][1]]
+                                * (avancement[0][0] * weigth[bonzes[0][1]] + avancement[1][0] * weigth[bonzes[1][1]]
+                                        + (avancement[2][0] + 2) * weigth[bonzes[2][1]]);
 
-        if (esperance >= progresActuelle/coefEsperance)
+        if (esperance >= progresActuel / coefEsperance)
             output = false;
 
         if (this.debug) {
-            System.out.println("esprance = " + esperance + " progresActuelle = " + progresActuelle/coefEsperance + " diff = "
-                    + (esperance - progresActuelle/coefEsperance));
+            System.out.println("esprance = " + esperance + " progresActuelle = " + progresActuel/ coefEsperance
+                    + " diff = " + (esperance - progresActuel / coefEsperance));
             System.out.println("Stop ? : = " + output);
             System.out.println("-------------------------------------------------------------------");
         }
@@ -346,6 +363,10 @@ public class Strat33 implements Strategie {
 
     }
 
+    /**
+     *  fonction d'affichage de tableau de int, utilisé pour le debug 
+     * @param tab tableau à afficher
+     */
     public static void afficherTabInt(int[] tab) {
         for (int i = 0; i < tab.length; i++)
             System.out.print(tab[i] + " | ");
