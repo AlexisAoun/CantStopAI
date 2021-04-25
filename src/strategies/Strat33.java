6c097464290528f1ce62b66c2c52b3078c124284
Alexis Aoun <pizzaman@localhost.localdomain>
2021-04-25 10:58:42 +0200

    fix: Fixed compute proba

--- src/strategies/Strat33.java
+++ src/strategies/Strat33.java
@@ -19,7 +19,6 @@ public class Strat33 implements Strategie {
     private double probaTriple[][][];
     private int playCount;
 
-    private boolean debug = true;
 
     public Strat33() {
         this.probaSingle = new double[13];
@@ -33,7 +32,7 @@ public class Strat33 implements Strategie {
 
     // Fonction qui calcule toutes les proba dont on aura besoin pour le reste
     public void computeProba() {
-        int[] sumD = new int[6];
+        int[][] sumD = new int[3][2];
 
         // On commence par compter toutes les issues
         for (int a = 1; a < 7; a++) {
@@ -49,11 +48,11 @@ public class Strat33 implements Strategie {
                             if (occurences > 0)
                                 this.probaSingle[i]++;
 
-                            for (int j = i+1; j <= 12; j++) {
+                            for (int j = i + 1; j <= 12; j++) {
                                 if (isCoupleInSum(sumD, i, j))
                                     this.probaCouple[i][j]++;
 
-                                for (int k = j+1; k <= 12; k++) {
+                                for (int k = j + 1; k <= 12; k++) {
                                     if (isOneOfThreeInSum(sumD, i, j, k))
                                         this.probaTriple[i][j][k]++;
                                 }
@@ -67,64 +66,68 @@ public class Strat33 implements Strategie {
 
         double nbTotalIssues = Math.pow(6, 4);
 
-
         for (int i = 2; i <= 12; i++) {
 
             this.freq[i] /= nbTotalIssues;
-            this.weigth[i] = 1/this.freq[i];
+            this.weigth[i] = 1 / this.freq[i];
 
             this.probaSingle[i] /= nbTotalIssues;
 
-            for (int j = i+1; j <= 12; j++) {
+            for (int j = i + 1; j <= 12; j++) {
 
                 this.probaCouple[i][j] /= nbTotalIssues;
 
-                    for(int k = j+1; k <= 12; k++)
-                        this.probaTriple[i][j][k] /= nbTotalIssues;
-                    
+                for (int k = j + 1; k <= 12; k++)
+                    this.probaTriple[i][j][k] /= nbTotalIssues;
+
             }
         }
     }
 
     // Fonction qui return un tableau avec toutes les sommes possbiles de 4 des
-    public int[] computeSumD(int a, int b, int c, int d) {
-        int[] output = new int[6];
-        output[0] = a + b;
-        output[1] = a + c;
-        output[2] = a + d;
-        output[3] = b + c;
-        output[4] = b + d;
-        output[5] = c + d;
+    public int[][] computeSumD(int a, int b, int c, int d) {
+        int[][] output = new int[3][2];
+
+        output[0][0] = a + b;
+        output[0][1] = c + d;
+        output[1][0] = a + c;
+        output[1][1] = b + d;
+        output[2][0] = a + d;
+        output[2][1] = b + c;
 
         return output;
     }
 
     // Fonction qui retourne le nombre de fois que n est present dans le tableau de
     // somme
-    public int computeOccurences(int[] sumD, int n) {
+    public int computeOccurences(int[][] sumD, int n) {
         int output = 0;
-        for (int i = 0; i < 6; i++) {
-            if (sumD[i] == n)
-                output++;
+        for (int i = 0; i < 3; i++) {
+            if (sumD[i][0] == n && sumD[i][1] == n)
+                output = 2;
+
+            else if (sumD[i][0] == n || sumD[i][1] == n)
+                output = 1;
         }
+
         return output;
     }
 
     // Fonction qui verifie si un couple d entier se trouve dans le tableau de somme
-    public boolean isCoupleInSum(int[] sumD, int n1, int n2) {
+    public boolean isCoupleInSum(int[][] sumD, int n1, int n2) {
         boolean output = false;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if ((sumD[i] == n1 && sumD[j] == n2) && i != j)
-                    output = true;
-            }
+        for (int i = 0; i < 3; i++) {
+            if ((sumD[i][0] == n1 && sumD[i][1] == n2) || (sumD[i][0] == n2 && sumD[i][1] == n1) )
+                output = true;
         }
         return output;
     }
 
+    
+
     // Fonction qui verifie que au moins une des trois valeurs est presente dans le
     // tableau de somme
-    public boolean isOneOfThreeInSum(int[] sumD, int n1, int n2, int n3) {
+    public boolean isOneOfThreeInSum(int[][] sumD, int n1, int n2, int n3) {
         boolean output = false;
         if (((computeOccurences(sumD, n1) != 0) || (computeOccurences(sumD, n2) != 0))
                 || (computeOccurences(sumD, n3) != 0))
