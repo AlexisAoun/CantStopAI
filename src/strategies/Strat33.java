18966cb47ed45b5ad85087492c5f5db99752ff64
Alexis Aoun <pizzaman@localhost.localdomain>
2021-04-25 16:03:06 +0200

    feat: choix first implementation

--- src/strategies/Strat33.java
+++ src/strategies/Strat33.java
@@ -16,15 +16,18 @@ public class Strat33 implements Strategie {
 
     private double probaSingle[], freq[], weigth[];
     private double probaCouple[][];
+    private double probaDouble[][];
     private double probaTriple[][][];
     private int playCount;
 
+    private boolean debug = true;
 
     public Strat33() {
         this.probaSingle = new double[13];
         this.freq = new double[13];
         this.weigth = new double[13];
         this.probaCouple = new double[13][13];
+        this.probaDouble = new double[13][13];
         this.probaTriple = new double[13][13][13];
 
         computeProba();
@@ -51,6 +54,8 @@ public class Strat33 implements Strategie {
                             for (int j = i + 1; j <= 12; j++) {
                                 if (isCoupleInSum(sumD, i, j))
                                     this.probaCouple[i][j]++;
+                                if (isOneOfTwoInSum(sumD, i, j))
+                                    this.probaDouble[i][j]++;
 
                                 for (int k = j + 1; k <= 12; k++) {
                                     if (isOneOfThreeInSum(sumD, i, j, k))
@@ -76,6 +81,7 @@ public class Strat33 implements Strategie {
             for (int j = i + 1; j <= 12; j++) {
 
                 this.probaCouple[i][j] /= nbTotalIssues;
+                this.probaDouble[i][j] /= nbTotalIssues;
 
                 for (int k = j + 1; k <= 12; k++)
                     this.probaTriple[i][j][k] /= nbTotalIssues;
@@ -117,13 +123,18 @@ public class Strat33 implements Strategie {
     public boolean isCoupleInSum(int[][] sumD, int n1, int n2) {
         boolean output = false;
         for (int i = 0; i < 3; i++) {
-            if ((sumD[i][0] == n1 && sumD[i][1] == n2) || (sumD[i][0] == n2 && sumD[i][1] == n1) )
+            if ((sumD[i][0] == n1 && sumD[i][1] == n2) || (sumD[i][0] == n2 && sumD[i][1] == n1))
                 output = true;
         }
         return output;
     }
 
-    
+    public boolean isOneOfTwoInSum(int[][] sumD, int n1, int n2) {
+        boolean output = false;
+        if ((computeOccurences(sumD, n1) != 0) || (computeOccurences(sumD, n2) != 0))
+            output = true;
+        return output;
+    }
 
     // Fonction qui verifie que au moins une des trois valeurs est presente dans le
     // tableau de somme
@@ -140,7 +151,69 @@ public class Strat33 implements Strategie {
      * @return
      */
     public int choix(Jeu j) {
-        return 0;
+
+        int output = 0;
+
+        if (j.getNbChoix() == 1)
+            return 0;
+
+        double scoreMax = 0;
+        int[] nbCases = j.getMaximum();
+
+        if (this.debug)
+            System.out.println("--------------------- Choix Debug ---------------------");
+
+        for (int i = 0; i < j.getNbChoix(); i++) {
+            double probaColonne1 = 0;
+            double probaColonne2 = 0;
+
+            double score = 0;
+
+            int[][] choix = j.getLesChoix();
+            probaColonne1 = probaFinirUneColonne(j, choix[i][0], j.avancementJoueurEnCours()[choix[i][0] - 2]);
+
+            if (choix[i][0] != 0)
+                probaColonne2 = probaFinirUneColonne(j, choix[i][1], j.avancementJoueurEnCours()[choix[i][1] - 2]);
+
+            int[] avancementAdverse = j.avancementAutreJoueur();
+            int casesRestantesAdvC1 = nbCases[choix[i][0] - 2] - avancementAdverse[choix[i][0] - 2];
+            int casesRestantesAdvC2 = nbCases[choix[i][1] - 2] - avancementAdverse[choix[i][1] - 2];
+
+            int[] avancementIA = j.avancementJoueurEnCours();
+            int casesRestantesC1 = nbCases[choix[i][0] - 2] - avancementIA[choix[i][0] - 2];
+            int casesRestantesC2 = nbCases[choix[i][1] - 2] - avancementIA[choix[i][1] - 2];
+
+            if (casesRestantesAdvC1 * weigth[choix[i][0]] - casesRestantesC1 * weigth[choix[i][0]] > 5)
+                probaColonne1 /= 2;
+
+            if (casesRestantesAdvC2 * weigth[choix[i][1]] - casesRestantesC2 * weigth[choix[i][1]] > 5)
+                probaColonne2 /= 2;
+
+
+            score = (probaColonne1 + probaColonne2) * 10;
+
+            if (this.debug) {
+                System.out.println("Choix Actuel i = " + i + " proba colonne " + choix[i][0] + " = " + probaColonne1
+                        + " proba colonne " + choix[i][1] + " = " + probaColonne2);
+                System.out.println("Score du choix = "+score+" scoreMax = "+scoreMax);
+            }
+
+            if (score >= scoreMax) {
+                scoreMax = score;
+                output = i;
+            }
+        }
+
+        return output;
+    }
+
+    public double probaFinirUneColonne(Jeu j, int colonne, int avancement) {
+        double output = 0;
+        int casesRestantes = j.getMaximum()[colonne - 2] - avancement;
+
+        output = Math.pow(this.probaSingle[colonne], casesRestantes * weigth[colonne]);
+
+        return output;
     }
 
     /**
